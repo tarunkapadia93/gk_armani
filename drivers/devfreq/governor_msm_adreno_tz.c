41adfeca82e821c9463e8ca31e611b2d83e64294
Pranav Vashi <neobuddy89@gmail.com>
2015-06-24 19:24:10 +0530

    devfreq: adreno_idler: Clean up
    * Disable adreno idler by default.
    * Clean up code implementation - add header file for externs.
    * Merge updates from @Dorimanx.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    Signed-off-by: tarun93 <tarunmyid@gmail.com>
    
    Conflicts:
    	drivers/devfreq/governor_msm_adreno_tz.c
    
    Signed-off-by: tarun93 <tarunmyid@gmail.com>
    
    Conflicts:
    	drivers/devfreq/governor_msm_adreno_tz.c
    
    Signed-off-by: tarun93 <tarunmyid@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -22,6 +22,9 @@
 #include <linux/state_notifier.h>
 #include <mach/scm.h>
 #include "governor.h"
+#ifdef CONFIG_ADRENO_IDLER
+#include "adreno_idler.h"
+#endif
 
 static DEFINE_SPINLOCK(tz_lock);
 
@@ -58,6 +61,9 @@ static DEFINE_SPINLOCK(tz_lock);
 
 #define TAG "msm_adreno_tz: "
 
+/* Boolean to detect if pm has entered suspend mode */
+static bool suspended;
+
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
 {
@@ -92,11 +98,6 @@ static void _update_cutoff(struct devfreq_msm_adreno_tz_data *priv,
 		priv->bus.down[i] = priv->bus.p_down[i] * norm_max / 100;
 	}
 }
-
-#ifdef CONFIG_ADRENO_IDLER
-extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
-		 unsigned long *freq);
-#endif
 
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				u32 *flag)
@@ -135,13 +136,14 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
 	 */
-	if (state_suspended) {
+	if (suspended || state_suspended) {
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
 
 #ifdef CONFIG_ADRENO_IDLER
-	if (adreno_idler(stats, devfreq, freq)) {
+	if (adreno_idler_active &&
+			adreno_idler(stats, devfreq, freq)) {
 		/* adreno_idler has asked to bail out now */
 		return 0;
 	}
@@ -340,6 +342,8 @@ static int tz_resume(struct devfreq *devfreq)
 	struct devfreq_dev_profile *profile = devfreq->profile;
 	unsigned long freq;
 
+	suspended = false;
+
 	freq = profile->initial_freq;
 
 	return profile->target(devfreq->dev.parent, &freq, 0);
@@ -348,6 +352,8 @@ static int tz_resume(struct devfreq *devfreq)
 static int tz_suspend(struct devfreq *devfreq)
 {
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
+
+	suspended = true;
 
 	__secure_tz_entry2(TZ_RESET_ID, 0, 0);
 
